459d13c943e2ae11c22a8bbb8d8c25ef3483c9d4
Imad Saddik <simad3647@gmail.com>
2024-01-22 15:07:12 +0100

    Added a modal to change the room name in the home activity

--- app/src/main/java/com/example/ragapplication/MainActivity.java
+++ app/src/main/java/com/example/ragapplication/MainActivity.java
@@ -5,8 +5,14 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.OpenableColumns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +20,7 @@ import androidx.annotation.Nullable;
 
 import android.view.MotionEvent;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.GestureDetectorCompat;
@@ -23,6 +30,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 import com.tom_roush.pdfbox.android.PDFBoxResourceLoader;
 
 import java.io.FileNotFoundException;
@@ -35,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     private GestureDetectorCompat gestureDetector;
     private ImageView leftNavigationDrawerIcon, rightNavigationDrawerIcon;
     private ImageButton uploadFilesButton;
+    private TextView roomNameTextView;
+    private RelativeLayout roomNameBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +83,14 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[]{"text/plain", "application/pdf"});
             startActivityForResult(intent, 1);
         });
+
+        roomNameTextView.setOnClickListener(v -> {
+            showInputDialog();
+        });
+
+        roomNameBackground.setOnClickListener(v -> {
+            showInputDialog();
+        });
     }
 
     private void instatiateViews() {
@@ -82,6 +100,8 @@ public class MainActivity extends AppCompatActivity {
         leftNavigationDrawerIcon = findViewById(R.id.leftNavigationDrawerIcon);
         rightNavigationDrawerIcon = findViewById(R.id.rightNavigationDrawerIcon);
         uploadFilesButton = findViewById(R.id.uploadFilesButton);
+        roomNameTextView = findViewById(R.id.roomNameTextView);
+        roomNameBackground = findViewById(R.id.roomNameBackground);
     }
 
     @Override
@@ -149,5 +169,29 @@ public class MainActivity extends AppCompatActivity {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    private void showInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.room_name_dialog, null);
+        builder.setView(view);
+
+        TextInputEditText editText = view.findViewById(R.id.roomNameEditText);
+        Button cancelButton = view.findViewById(R.id.cancelButton);
+        Button confirmButton = view.findViewById(R.id.confirmButton);
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.rounded_background_room_dialog);
+
+        cancelButton.setOnClickListener(v -> dialog.dismiss());
+
+        confirmButton.setOnClickListener(v -> {
+            String newText = editText.getText().toString();
+            roomNameTextView.setText(newText);
+            dialog.dismiss();
+        });
+
+        dialog.show();
     }
 }
